9a42b204d80862a64b91f38c94d3daa76d5a5469
ks-uppercut <tankangsoon@gmail.com>
2015-05-05 01:04:25 +0800

    facebook login test 3

--- fblogin.js
+++ fblogin.js
@@ -73,6 +73,6 @@
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+        'Hello, ' + response.name + '!';
     });
   }
